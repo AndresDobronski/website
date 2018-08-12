6c9a8ae32b2f7b5f918b89456c19fb0890bd1202
Tristan Kimball <ttkimball1@gmail.com>
2018-08-11 19:51:32 -0700

    add temporary website construction page, fix favicon

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,49 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        website
-      </h1>
-      <h2 class="subtitle">
-        My tiptop Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div>
+    <img class="banner" src="/banner.png" alt="ASU Esports Association">
+    <p class="text">
+      The website is currently under construction.
+    </p>
+    <a href="https://discord.gg/asu" class="link">
+      But you can still join our Discord server!
+    </a>
+    <iframe class="discord" src="https://discordapp.com/widget?id=198240842626891776&theme=dark" allowtransparency="true" frameborder="0" />
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+  export default {
 
-export default {
-  components: {
-    Logo
   }
-}
 </script>
 
 <style>
-.container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
-  padding-top: 15px;
-}
+  .banner {
+    display: block;
+    margin: 0 auto;
+    margin-top: -15vh;
+    height: 65vh;
+  }
+  .text {
+    display: block;
+    margin-top: -12vh;
+    text-align: center;
+    font-size: 30px;
+  }
+  .link {
+    display: block;
+    color: rgb(140, 29, 64);
+    text-decoration: none;
+    text-align: center;
+    font-size: 30px;
+    font-weight: 500;
+  }
+  .discord {
+    display: block;
+    margin-top: 5vh;
+    margin-left: auto;
+    margin-right: auto;
+    width: 75vh;
+    height: 40vh;
+  }
 </style>
